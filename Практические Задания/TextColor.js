ded363b5b0f0e1c377dda005ba93b40a7a67491f
JabrailMataev <mataevdhabrail200@gmail.com>
2025-05-31 14:59:05 +0300

    Delete Практические Задания directory

--- Практические Задания/TextColor.js
+++
@@ -1,8 +0,0 @@
- const colorCells = document.querySelectorAll('.color-cell');
-  const textElement = document.querySelector('.text');
-    colorCells.forEach(cell => {
-     cell.addEventListener('click', function() {
-      const color = this.getAttribute('data-color');
-       textElement.style.color = color;
-            });
-        });
